e8d94558be47a123298168230f68c459569df104
Joe Teichman <kaalm1@gmail.com>
2017-05-22 18:56:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newHash = {}
+  data.each do |general,info|
+    info.each do |key,value|
+      value.each do |x|
+        newHash[x] = {} if newHash[x].nil?
+        if newHash[x][general].nil?
+          newHash[x].merge!({general => [key.to_s]}) 
+        else
+          newHash[x][general].push(key.to_s)
+        end
+      end
+    end
+  end
+  newHash
 end
